b7a2c0154978b5f62ba0df097dab74094c9be270
Olivier Diotte <olivier@diotte.ca>
2021-01-28 01:47:59 -0500

    Add and use Row and RowChange classes

---
+++ Row.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace SDF_comparator {
+    class Row : IEnumerable {
+        private object[] raw;
+        public Row(object[] raw) {
+            this.raw = raw.Clone() as object[];
+        }
+        public int Length { get { return raw.Length; } }
+        public object this[int i] { get { return raw[i]; } }
+        public IEnumerator GetEnumerator() {
+            return raw.GetEnumerator();
+        }
+        public override string ToString() {
+            string s = "";
+            string sep = "";
+            foreach (var col in raw) {
+                s += $"{sep}{col}";
+                sep = " | ";
+            }
+            return s;
+        }
+    }
+}
