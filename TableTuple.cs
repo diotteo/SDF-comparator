08f4ecca724782b1b9f8c7ab1f016bcb5cd18a2b
Olivier Diotte <olivier@diotte.ca>
2021-03-17 21:03:29 -0400

    Move build_row_dicts(), prune_full_matches() and build_row_changes() into TableTuple

--- TableTuple.cs
+++ TableTuple.cs
@@ -101,5 +101,157 @@ namespace SDF_comparator {
             cmd.CommandText = s;
             return cmd.ExecuteReader();
         }
+
+        public List<RowChange> get_row_changes() {
+            /* A list of dictionaries each containing a collection of rows with matching column values
+             * row_dicts[1][col_val][3] means "get the 4th row whose 2nd column has a value of col_var"
+             */
+            Parent.Orig.Connection.Open();
+            var row_dicts = build_row_dicts(OrigReader);
+            Parent.Orig.Connection.Close();
+
+            Parent.Dest.Connection.Open();
+            var dest_rows = prune_full_matches(DestReader, row_dicts);
+            Parent.Dest.Connection.Close();
+
+            return build_row_changes(row_dicts, dest_rows);
+        }
+
+        private List<Dictionary<object, List<Row>>> build_row_dicts(SqlCeDataReader rdr) {
+            var row_dicts = new List<Dictionary<object, List<Row>>>();
+            object[] raw_row = new object[rdr.FieldCount];
+
+            /* Create the empty dictionaries, even if there is no row in the table,
+                * just so we don't have to deal with that edge case elsewhere
+                */
+            for (int i = 0; i < rdr.FieldCount; i++) {
+                row_dicts.Add(new Dictionary<object, List<Row>>());
+            }
+
+            while (rdr.Read()) {
+                rdr.GetValues(raw_row);
+                var cur_row = new Row(raw_row);
+                for (int j = 0; j < cur_row.Length; j++) {
+                    if (!row_dicts[j].ContainsKey(cur_row[j])) {
+                        row_dicts[j].Add(cur_row[j], new List<Row>());
+                    }
+                    row_dicts[j][cur_row[j]].Add(cur_row);
+                }
+            }
+
+            return row_dicts;
+        }
+
+        private List<Row> prune_full_matches(SqlCeDataReader rdr, List<Dictionary<object, List<Row>>> row_dicts) {
+            var dest_rows = new List<Row>();
+            object[] raw_row = new object[rdr.FieldCount];
+            while (rdr.Read()) {
+                rdr.GetValues(raw_row);
+                var cur_row = new Row(raw_row);
+
+                /* Remove the first full match
+                 * A full match is that of the first column matches that also matches every other column
+                 * We can't just check for a match of every column since multiple rows could each match on a single value
+                 */
+                var pot_full_matches = new List<Row>();
+                if (row_dicts[0].TryGetValue(cur_row[0], out List<Row> first_col_matches)) {
+                    bool b_had_full_match = false;
+                    foreach (var pot_match in first_col_matches) {
+                        bool b_is_full_match = true;
+                        for (int j = 1; j < pot_match.Length; j++) {
+                            if (!pot_match[j].Equals(cur_row[j])) {
+                                b_is_full_match = false;
+                                break;
+                            }
+                        }
+
+                        //If full match, remove match from every column dict
+                        if (b_is_full_match) {
+                            b_had_full_match = true;
+                            for (int j = 0; j < row_dicts.Count; j++) {
+                                row_dicts[j][cur_row[j]].Remove(pot_match);
+                            }
+                            break;
+                        }
+                    }
+
+                    if (!b_had_full_match) {
+                        dest_rows.Add(cur_row);
+                    }
+                } else {
+                    dest_rows.Add(cur_row);
+                }
+            }
+            return dest_rows;
+        }
+
+        private List<RowChange> build_row_changes(List<Dictionary<object, List<Row>>> row_dicts, List<Row> dest_rows) {
+            var changes = new List<RowChange>();
+
+            var pot_matches = new HashSet<Row>();
+            var unmatched_dst_rows = new List<Row>();
+
+            foreach (var dst_row in dest_rows) {
+                //For each row value, gather all rows that match on at least that column value, without duplicates
+                pot_matches.Clear();
+                for (int i = 0; i < row_dicts.Count; i++) {
+                    var row_dict = row_dicts[i];
+                    if (row_dict.TryGetValue(dst_row[i], out List<Row> idx_rows)) {
+                        pot_matches.UnionWith(idx_rows);
+                    }
+                }
+
+                Row best_match = null;
+                List<int> best_match_idxs = null;
+                var col_matches = new List<int>();
+                foreach (var pot_match in pot_matches) {
+                    col_matches.Clear();
+                    for (int i = 0; i < pot_match.Length; i++) {
+                        if (dst_row[i].Equals(pot_match[i])) {
+                            col_matches.Add(i);
+                        }
+                    }
+                    if (col_matches.Count > 0
+                            && (best_match_idxs is null || best_match_idxs.Count < col_matches.Count)) {
+                        best_match_idxs = new List<int>(col_matches);
+                        best_match = pot_match;
+                    }
+                }
+                if (best_match is null) {
+                    unmatched_dst_rows.Add(dst_row);
+                } else {
+                    var diff_idxs = new List<int>();
+
+                    int start_idx = 0;
+                    foreach (var match_idx in best_match_idxs.Union(new int[] { best_match_idxs.Count })) {
+                        for (int i = start_idx; i < match_idx; i++) {
+                            diff_idxs.Add(i);
+                        }
+                        start_idx = match_idx + 1;
+                    }
+
+                    changes.Add(new RowChange(best_match, dst_row, diff_idxs));
+                    for (int i = 0; i < row_dicts.Count; i++) {
+                        //use best_match[i] instead of dst_row[i] since we're doing partial matches, so the match is indexed elsewhere in some columns
+                        row_dicts[i][best_match[i]].Remove(best_match);
+                    }
+                }
+            }
+
+            /* row_dicts is a list of {column_count} dictionaries of rows, indexed by their values for each column
+             * Here, we just want to add all rows that have at least one change to changes, so we just go through the first dictionary
+             * (because it doesn't matter which one we use).
+             */
+            foreach (var row_pair in row_dicts[0]) {
+                foreach (var row in row_pair.Value) {
+                    changes.Add(new RowChange(row, null));
+                }
+            }
+
+            foreach (var row in unmatched_dst_rows) {
+                changes.Add(new RowChange(null, row));
+            }
+            return changes;
+        }
     }
 }
